a20ac6f8fd462b4f61a008cb7408d737772fb730
Rafael <rafaelproinfo@gmail.com>
2022-07-25 11:25:08 -0300

    Update Tarefa

--- todolist/src/tarefa/services/tarefa.service.ts
+++ todolist/src/tarefa/services/tarefa.service.ts
@@ -1,6 +1,6 @@
 import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
-import { DeleteResult, ILike, Repository } from "typeorm";
+import { Repository } from "typeorm";
 import { Tarefa } from "../entities/tarefa.entity";
 
 @Injectable()
